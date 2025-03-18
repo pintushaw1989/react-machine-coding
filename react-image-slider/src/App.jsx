9b0471881341c80c2431166b39e263fdab3bf7dd
pintushaw1989 <pintushaw1989@gmail.com>
2025-03-19 00:32:15 +0530

    image slider react starter code

---
+++ react-image-slider/src/App.jsx
@@ -0,0 +1,8 @@
+import ImageSlider from "./components/ImageSlider";
+import "./App.css";
+
+function App() {
+  return <ImageSlider />;
+}
+
+export default App;
---
+++ react-image-slider/src/components/ImageSlider.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ImageSlider.css";
+
+const ImageSlider = () => {
+  return <div>ImageSlider</div>;
+};
+
+export default ImageSlider;
